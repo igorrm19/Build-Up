19a8204877c8294ec7a63c3f61b1c8c8ab5154d0
igorrm23 <igormachadofire14@gmail.com>
2026-01-19 17:44:06 -0300

    Adicionando tarefa 5

--- server.js
+++ server.js
@@ -3,16 +3,29 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-const nome = "Igor";
+let tempBooks = ["The Hobbit", "1984"]
 
 
 app.get("/", (req, res) => {
-    res.send("Hello World! ", nome)
+    res.send("Hello World! ")
 });
 
 
 app.get("/status", (req, res) => {
     res.json({ status: "online", message: "Ready to log books" })
+});
+
+
+app.get("/tests-book", (req, res) => {
+    res.json(tempBooks);
+
+    return tempBooks;
+});
+
+app.post("/tests-book", (req, res) => {
+    const book = req.body;
+    tempBooks.push(book);
+    res.json(book);
 });
 
 
